fdbd61ae75010c6670e8d2ee3269f7f4a8e71967
SirVoRaK <sirvoraky@gmail.com>
2021-10-24 19:52:41 -0300

    new register page

--- account/main.js
+++ account/main.js
@@ -4,32 +4,11 @@
     const login_form = document.querySelector('.login')
     const login_email = document.querySelector('.login input[name="email"]')
     const login_password = document.querySelector('.login input[name="password"]')
-    const login_submit = document.querySelector('.login input[name="submit"]')
 
-    const register_form = document.querySelector('.register')
-    const register_email = document.querySelector('.register input[name="email"]')
-    const register_name = document.querySelector('.register input[name="name"]')
-    const register_password = document.querySelector('.register input[name="password"]')
-    const register_submit = document.querySelector('.register input[name="submit"]')
 
     login_form.onsubmit = e => {
         e.preventDefault()
         login(login_email.value, login_password.value)
-    }
-    register_form.onsubmit = e => {
-        e.preventDefault()
-        register(new User(register_email.value, register_name.value, register_password.value))
-    }
-
-    class User {
-        email
-        name
-        password
-        constructor(email, name, password) {
-            this.email = email
-            this.name = name
-            this.password = password
-        }
     }
 
     function login(u, p) {
@@ -60,34 +39,9 @@
         })
     }
 
-
-    function register(user) {
-        fetch(url + "/users", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(user)
-            }).then((res) => {
-                console.log(res)
-                return res.json()
-            })
-            .then(result => {
-                if (result == undefined) {
-                    console.log('usuario ja cadastrado')
-                    return
-                }
-                console.log(result)
-            }).catch((err) => {
-                console.error(err)
-            })
-    }
-
     const key = 'supersecretkey17845'
 
     function encrypt(msg) {
         return CryptoJS.AES.encrypt(msg, key).toString()
     }
-
-
 })()
